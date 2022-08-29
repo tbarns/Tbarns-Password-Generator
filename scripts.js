6d167897a401de0ccc74903e464c5623b8145646
Timothy Barnaby <tbarnaby1@gmail.com>
2022-08-28 22:00:14 -0500

    updated JS

--- scripts.js
+++ scripts.js
@@ -1,38 +1,40 @@
+// cleaner version of script + s if i want to use this 
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialChar = ["!", "@", "#", "$", "%", "&", "*", "_", "+", "="]; 
 var numbers =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var passwordLength = window.prompt("how long?")
-
-console.log(passwordLength)
 
 
-var generatePassword = function(){
-
-
-  if (passwordLength > 8 || passwordLength < 128) {
-    return};
-
-  if
-}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+function generatePassword() {
+    var passwordLength = window.prompt("How many characters would you like to use?")
+      
+    if(passwordLength >= 7),
+     (passwordLength <= 129); {
+       window.prompt("INVALID INPUT! Password must contain between 8 - 128 charecters.")
+    }else{
+    }
 
-//there needs to be logical statemtns that inlcude or exldue the 4 types of charecters and those need to be added to password text variabele
-  // if (passwordLength )
+    {
+ window.confirm("Would you like to use UPPERCASE?");
 
+ window.confirm("Would you like to use lowercase?");
+
+ window.confirm("Would you like to use numbers?");
+
+ window.confirm("Would you like to use special charecters?");
+    }
   
-
-
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+  }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
