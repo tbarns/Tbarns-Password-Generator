711ff8c5041aeec798a444ae429d31d2766a6fca
Timothy Barnaby <tbarnaby1@gmail.com>
2022-08-30 13:23:32 -0500

    updated JS to contain vars that establish user choices

--- scripts.js
+++ scripts.js
@@ -10,33 +10,48 @@ var numbers =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
 
+function generatePassword() {
+
+ var passwordPromt = window.prompt("How many characters would you like to use?")
+
+  var passwordLength = parseInt(passwordPromt)
+
+  if(passwordLength < 8 || passwordLength > 128) {
+   window.alert("INVALID INPUT! Password must contain between 8 - 128 charecters.")
+    return
+ } 
+    
+    
+ var upperConfirm = window.confirm("Would you like to use UPPERCASE?");
+ if(upperConfirm === true) 
+ console.log(upperConfirm)
+
+
+
+ var lowerConfirm = window.confirm("Would you like to use lowercase?");
+
+ console.log(lowerConfirm)
+
+ var numberConfirm = window.confirm("Would you like to use numbers?");
+
+ console.log(numberConfirm)
+
+ var specialConfirm = window.confirm("Would you like to use special characters?");
+
+ console.log(specialConfirm)
+    
+  
+  }
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-function generatePassword() {
-    var passwordLength = window.prompt("How many characters would you like to use?")
-      console.log(passwordLength)
-    if(passwordLength > 8 || passwordLength < 128) {
-       window.prompt("INVALID INPUT! Password must contain between 8 - 128 charecters.")
-    } 
-    else{
-    }
-
-    {
- window.confirm("Would you like to use UPPERCASE?");
-
-
- window.confirm("Would you like to use lowercase?");
-
- window.confirm("Would you like to use numbers?");
-
- window.confirm("Would you like to use special characters?");
-    }
-  
-  }
-// Add event listener to generate button
+  // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
 
